edaac7aa0b25e679c545ea4fbc5b5c4ba8e91d51
linukc <35877675+linukc@users.noreply.github.com>
2020-09-30 19:47:13 +0300

    add printing info

--- engine2inference.py
+++ engine2inference.py
@@ -4,7 +4,8 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 import numpy as np
 import segmentation_models as sm #вытянуть функцию обработки чтобы не надо делать импорт
-
+import pyrealsense2 as rs
+from pyrealsense2 import rs2_deproject_pixel_to_point as convert
 
 def load_engine(trt_runtime, plan_path):
     with open(plan_path, 'rb') as f:
@@ -74,8 +75,8 @@ def do_inference(engine, pics_1, h_input_1, d_input_1, h_output, d_output, strea
         cuda.memcpy_htod_async(d_input_1, h_input_1, stream)
 
         # Run inference.
-
-        context.profiler = trt.Profiler()
+	#context.Profiler()
+        #context.profiler = trt.Profiler()
         context.execute(batch_size=1, bindings=[int(d_input_1), int(d_output)])
 
         # Transfer predictions back from the GPU.
@@ -86,24 +87,98 @@ def do_inference(engine, pics_1, h_input_1, d_input_1, h_output, d_output, strea
         out = h_output.reshape((batch_size, height, width, -1))
         return out
 
+def probability2explicit_bin_mask(border, *args):
+	res = []
+	for mask in args:
+		_, mask = cv2.threshold(mask, border, 255, cv2.THRESH_BINARY)
+		res.append(mask)
+	return res
+
+def printing_info(min_area, depth, params, *args):
+	objects = ['beam', 'shaft', 'sleeve', 'square', 'kremlin']
+	for img, obj in zip(args, objects):
+		contours, _ = cv2.findContours(np.array(img, dtype=np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+		for cnt in contours:
+			(center_x, center_y), (length_x, length_y), angle = cv2.minAreaRect(cnt)
+			center_x, center_y = int(center_x), int(center_y)
+			center_z = depth[center_x][center_y]
+			area = int(length_x * length_y)
+			if area > min_area:
+				x, y, z = convert(params, [center_x, center_y], center_z)
+				print('-----------', obj, '-----------')
+				print('CenterX -', x)
+				print('CenterY -', y)
+				print('CenterZ -', z)
+				print('Angle -', angle)
+				print('-------------------------------')
 
 TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
 trt_runtime = trt.Runtime(TRT_LOGGER)
-
-input_file_path = '30_Color.png'
 serialized = "modelv4.engine"
-HEIGHT = 480
-WIDTH = 640
-
 preproc = sm.get_preprocessing('efficientnetb3')
-image = preproc(cv2.imread(input_file_path, cv2.COLOR_BGR2RGB))
-
+print('---STARTING LOAD ENGINE---')
 engine = load_engine(trt_runtime, serialized)
-h_input, d_input, h_output, d_output, stream = allocate_buffers(engine, 1, trt.float32)
-predict = do_inference(engine, image, h_input, d_input, h_output, d_output, stream, 1, HEIGHT, WIDTH)
-print(predict.shape)
+print('---STARTING PIPELINE---')
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.rgb8, 30)
+profile = pipeline.start(config)
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+align_to = rs.stream.color
+align = rs.align(align_to)
+
+output = np.ones((480, 640))
+print('---SKIPPING FEW FRAMES FOR CALIBRATION---')
+for i in range(20):
+	pipeline.wait_for_frames()
+
+print('---STARTING LOOP---')
 while True:
-	cv2.imshow('out', predict.squeeze()[..., 0])
-	if cv2.waitKey(1) & 0xFF == ord('q'):
+	frames = pipeline.wait_for_frames()
+	aligned_frames = align.process(frames)
+	aligned_depth_frame = aligned_frames.get_depth_frame()
+	color_frame = aligned_frames.get_color_frame()
+
+	if not aligned_depth_frame or not color_frame:
+		continue
+	key = cv2.waitKey(1)
+	if key & 0xFF == ord('p'):
+		print('p button pressed')
+		d_in = aligned_depth_frame.profile.as_video_stream_profile().intrinsics
+		depth_image = np.asanyarray(aligned_depth_frame.get_data())
+		color_image = np.asanyarray(color_frame.get_data())
+
+		h_input, d_input, h_output, d_output, stream = allocate_buffers(engine, 1, trt.float32)
+		predict = do_inference(engine, preproc(color_image), h_input, d_input, h_output, d_output, stream, 1, 480, 640)
+
+		predict = predict.squeeze()
+		beam = predict[..., 0]*255
+		shaft = predict[..., 1]*255
+		sleeve = predict[..., 2]*255
+		square = predict[..., 3]*255
+		kremlin = predict[..., 4]*255
+
+		beam, shaft, sleeve, square, kremlin = probability2explicit_bin_mask(225,
+					      beam,
+					      shaft,
+					      sleeve,
+					      square,
+					      kremlin)
+		printing_info(0, depth_image, d_in, beam, shaft, sleeve, square, kremlin)
+
+		output = np.hstack((np.resize(color_image, (192, 256)),
+					np.resize(beam, (192, 256)),
+					np.resize(shaft, (192, 256)),
+					np.resize(sleeve, (192, 256)),
+					np.resize(square, (192, 256)),
+					np.resize(kremlin, (192, 256))))
+
+	elif key & 0xFF == ord('q'):
+		print('q button pressed')
+		cv2.destroyAllWindows()
+		pipeline.stop()
 		break
-cv2.DestroyAllWindows()
+
+	cv2.imshow('Predict', output)
